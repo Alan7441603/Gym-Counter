40e38d9d7c598697a3cbe98734c89d5822da32d6
Alan Rivera <arivera@bsd220.org>
2023-12-06 09:34:04 -0600

    Initial Commit

---
+++ Gym Counter/Gym_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gym_CounterApp.swift
+//  Gym Counter
+//
+//  Created by Alan Rivera on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Gym_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
